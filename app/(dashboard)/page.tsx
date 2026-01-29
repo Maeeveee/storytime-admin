285480c53a3a6b9ddba34e8900ac98ee1b03a2e5
Maeeveee <142636445+Maeeveee@users.noreply.github.com>
2026-01-29 11:28:41 +0800

    feat: Create initial dashboard page and a reusable statistics card component.

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -2,14 +2,14 @@ import { CardDashbordStats } from "@/components/cards/dashboard/cardDashbordStat
 import { ChartPieDonutText } from "@/components/charts/dashboard/chartDashboardTotalStories";
 import { TableDashboardTopAuthor } from "@/components/tables/dashboard/tableDashboardTopAuthor";
 import { ChartDashboardGrowth } from "@/components/charts/dashboard/chartDashboardGrowth";
+import { BookOpenText } from "lucide-react";
 export default function DashboardPage() {
     return (
         <>
-            <div className="grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-4">
-                <CardDashbordStats title="tes" content="tes" />
-                <CardDashbordStats title="tes" content="tes" />
-                <CardDashbordStats title="tes" content="tes" />
-                <CardDashbordStats title="tes" content="tes" />
+            <div className="grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-3">
+                <CardDashbordStats title="Total Stories" content="100" icon={<BookOpenText />} />
+                <CardDashbordStats title="Total Categories" content="12" icon={<BookOpenText />} />
+                <CardDashbordStats title="Total Users" content="60" icon={<BookOpenText />} />
             </div>
             <div className="grid grid-cols-1 gap-2 md:grid-cols-2 lg:grid-cols-2">
                 <ChartPieDonutText />
--- components/cards/dashboard/cardDashbordStats.tsx
+++ components/cards/dashboard/cardDashbordStats.tsx
@@ -1,12 +1,21 @@
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 
-export const CardDashbordStats = ({ title, content }: { title: React.ReactNode; content: React.ReactNode }) => {
+export const CardDashbordStats = ({ title, content, icon }: { title: React.ReactNode; content: React.ReactNode; icon: React.ReactNode }) => {
 	return (
 		<Card>
 			<CardHeader>
 				<CardTitle>{title}</CardTitle>
 			</CardHeader>
-			<CardContent>{content}</CardContent>
+			<CardContent>
+				<div className="flex items-center justify-between">
+					<div>
+						<p className="text-2xl font-bold">{content}</p>
+					</div>
+					<div>
+						{icon}
+					</div>
+				</div>
+			</CardContent>
 		</Card>
 	);
 };
